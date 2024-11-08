7bdb406c06e30455f7c0df7bef2d527d7b7a771c
Madhumithaa5 <madhumithaachandran05@gmail.com>
2024-11-08 14:24:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World - Madhu - 22CB031
+          Hello World - Madhumithaa - 22CB031
         </p>
         <a
           className="App-link"
